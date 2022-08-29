22c68efbbb56cf4b549b9f162339089ea93fba94
Carlos Silva <carlospxsilva@gmail.com>
2022-08-29 09:19:13 -0300

    comitagem inicial do crud cadastro de professor

---
+++ src/main/java/br/com/xavecoding/regescweb/dto/RequisicaoFormProfessor.java
@@ -0,0 +1,75 @@
+package br.com.xavecoding.regescweb.dto;
+
+import br.com.xavecoding.regescweb.models.Professor;
+import br.com.xavecoding.regescweb.models.StatusProfessor;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+// É uma classe DTO (Data Transfer Object)
+public class RequisicaoFormProfessor {
+    @NotBlank
+    @NotNull
+    private String nome;
+    private StatusProfessor statusProfessor;
+    @NotNull
+    @DecimalMin(value="0.0")
+    private BigDecimal salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public StatusProfessor getStatusProfessor() {
+        return statusProfessor;
+    }
+
+    public void setStatusProfessor(StatusProfessor statusProfessor) {
+        this.statusProfessor = statusProfessor;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public Professor toProfessor(){
+        Professor professor = new Professor();
+        professor.setNome(this.nome);
+        professor.setSalario(this.salario);
+        professor.setStatusProfessor(this.statusProfessor);
+
+        return professor;
+    }
+    public Professor toProfessor(Professor professor){
+        professor.setNome(this.nome);
+        professor.setSalario(this.salario);
+        professor.setStatusProfessor(this.statusProfessor);
+
+        return professor;
+    }
+
+    public void fromProfessor(Professor professor){
+        this.nome = professor.getNome();
+        this.salario = professor.getSalario();
+        this.statusProfessor = professor.getStatusProfessor();
+    }
+
+    @Override
+    public String toString() {
+        return "RequisicaoNovoProfessor{" +
+                "nome='" + nome + '\'' +
+                ", statusProfessor=" + statusProfessor +
+                ", salario=" + salario +
+                '}';
+    }
+}
